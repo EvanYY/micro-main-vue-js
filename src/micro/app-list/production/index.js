bb67066b80c7237eedb9e3dc8f6a0ad37e0e67a2
EvanYY <evan_yangyang@leadscloud.com>
2020-07-27 15:31:59 +0800

    change cmd build

--- src/micro/app-list/production/index.js
+++ src/micro/app-list/production/index.js
@@ -7,7 +7,7 @@ export const microApp = [
   // },
   {
     name: 'VueMicroApp',
-    entry: 'http://192.168.74.216:10200',
+    entry: '//192.168.74.216:10200',
     container: '#frame',
     activeRule: '#/vue'
   }
