e581b6bd528ee25cb3d87c84bca2620c1f5bf288
EvanYY <evan_yangyang@leadscloud.com>
2020-08-04 16:50:00 +0800

    修改 dll 文件

--- vue.config.js
+++ vue.config.js
@@ -4,13 +4,22 @@ const webpack = require('webpack')
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
 // 导入缓存
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
-
 // 实例化插件
 const smp = new SpeedMeasurePlugin()
+const utilsResource = require('./public/dll/utilsResource.manifest.json')
+const vueResource = require('./public/dll/vueResource.manifest.json')
+const qiankunResource = require('./public/dll/qiankunResource.manifest.json')
+
 const eslintSorceMap = process.env.NODE_ENV !== 'production' || process.env.VUE_APP_MODE === 'test'
 function getExternals () {
   return {
-    lodash: '_'
+    lodash: '_',
+    vue: 'Vue',
+    vuex: 'Vuex',
+    axios: 'axios',
+    redux: 'Redux',
+    'vue-router': 'VueRouter',
+    qiankun: 'qiankun'
   }
 }
 module.exports = {
@@ -21,7 +30,8 @@ module.exports = {
         '@': path.resolve(__dirname, 'src')
       }
     },
-    externals: process.env.NODE_ENV === 'production' ? getExternals() : {},
+    externals: getExternals(),
+    // externals: process.env.NODE_ENV === 'production' ? getExternals() : {},
     optimization: {
       splitChunks: { // 分割代码块
         cacheGroups: {
@@ -52,17 +62,49 @@ module.exports = {
         }
       }
     },
-    plugins: [new webpack.DllReferencePlugin({
-      manifest: path.resolve(__dirname, process.env.NODE_ENV === 'production' ? 'public' : './public', 'dll', 'manifest.json')
-    })].concat(process.env.NODE_ENV === 'production' ? [
-      new HardSourceWebpackPlugin()
-    ] : [])
+    plugins: [
+      new webpack.DllReferencePlugin({
+        manifest: path.resolve(__dirname, 'public', 'dll', 'utilsResource.manifest.json')
+      }),
+      new webpack.DllReferencePlugin({
+        manifest: path.resolve(__dirname, 'public', 'dll', 'vueResource.manifest.json')
+      }),
+      new webpack.DllReferencePlugin({
+        manifest: path.resolve(__dirname, 'public', 'dll', 'qiankunResource.manifest.json')
+      })
+    ]
+      .concat(process.env.NODE_ENV === 'production' ? [
+        new HardSourceWebpackPlugin({
+          // cacheDirectory是在高速缓存写入。默认情况下，将缓存存储在node_modules下的目录中，因此如
+          // 果清除了node_modules，则缓存也是如此
+          cacheDirectory: 'node_modules/.cache/hard-source/[confighash]',
+          // Either an absolute path or relative to webpack's options.context.
+          // Sets webpack's recordsPath if not already set.
+          recordsPath: 'node_modules/.cache/hard-source/[confighash]/records.json',
+          // configHash在启动webpack实例时转换webpack配置，并用于cacheDirectory为不同的webpack配
+          // 置构建不同的缓存
+          configHash: function (webpackConfig) {
+            // node-object-hash on npm can be used to build this.
+            return require('node-object-hash')({ sort: false }).hash(webpackConfig)
+          },
+          // 当加载器，插件，其他构建时脚本或其他动态依赖项发生更改时，hard-source需要替换缓存以确保输
+          // 出正确。environmentHash被用来确定这一点。如果散列与先前的构建不同，则将使用新的缓存
+          environmentHash: {
+            root: process.cwd(),
+            directories: [],
+            files: ['package-lock.json', 'yarn.lock']
+          }
+        })
+      ] : [])
   }),
   chainWebpack: config => {
     config
       .plugin('html')
       .tap(args => {
         args[0].title = '询盘云系统管理'
+        args[0].utilsResource = utilsResource.name + '.js'
+        args[0].vueResource = vueResource.name + '.js'
+        args[0].qiankunResource = qiankunResource.name + '.js'
         return args
       })
     if (process.env.VUE_APP_MODE === 'test') {
--- webpack.config.dll.js
+++ webpack.config.dll.js
@@ -3,20 +3,24 @@ const webpack = require('webpack')
 
 module.exports = {
   entry: {
-    antDesignVue: ['ant-design-vue']
+    // antDesignVue: ['ant-design-vue'],
+    utilsResource: ['lodash'],
+    vueResource: ['vue', 'vuex', 'vue-router', 'axios'],
+    qiankunResource: ['qiankun', 'redux']
   },
+  mode: 'production',
   output: {
-    filename: '[name]_dll.js',
+    filename: '[name]_dLL.[chunkhash:8].js',
     path: path.resolve(__dirname, 'public', 'dll'),
-    library: '[name]_dll' // 暴露给外部使用
-    // libraryTarget 指定如何暴露内容，缺省时就是 var
+    library: '[name]_dll_[chunkhash:8]' // 暴露给外部使用
+    // libraryTarget: '' // 指定如何暴露内容，缺省时就是 var
   },
   plugins: [
     new webpack.DllPlugin({
       // name和library一致
-      name: '[name]_dll',
+      name: '[name]_dll_[chunkhash:8]',
       context: __dirname,
-      path: path.resolve(__dirname, 'public', 'dll', 'manifest.json') // manifest.json的生成路径
+      path: path.resolve(__dirname, 'public', 'dll', '[name].manifest.json') // manifest.json的生成路径
     })
   ]
 }
